02025eeca22b80af91d14032f260dda6d6d8c29a
botprzemek <botprzemekk@gmail.com>
2022-10-22 23:17:24 +0000

    User authentication

--- modules/user.js
+++ modules/user.js
@@ -1,13 +1,44 @@
-const database = require('../modules/database');
+const database = require('../modules/database'),
+      { scryptSync, timingSafeEqual } = require('crypto'),
+      { sign } = require('jsonwebtoken'),
+      regex = {
+        login    : /^[a-zA-Z0-9_@]{8,32}$/,
+        email    : /([A-Z]|[a-z]|[^<>()\[\]\\\/.,;:\s@"]){4,}\@([A-Z]|[a-z]|[^<>()\[\]\\\/.,;:\s@"]){4,}\.(com|net)/,
+        password : /^([a-zA-Z0-9@*#_!]{8,24})$/,
+      };
 
 async function addUser(user){
-    if (user.login.match('/^[a-zA-Z0-9_]{6,24}$/')) {
-        const query = `INSERT INTO login.users (login, email, password) VALUES ('${user.login}', '${user.email}', '${user.password}')`,
-              res = await database.sendQuery(query);
-        return res;
-    }// || (!user.email.match('/^[a-zA-Z0-9_@]{8,32}$/'))) return;
+    if ((!user.login.match(regex.login)) || (!user.email.match(regex.email)) || (!user.password.match(regex.password))) return false;
+    const query = `INSERT INTO login.users (login, email, password) VALUES ('${user.login}', '${user.email}', '${user.password}')`,
+          res = await database.sendQuery(query);
+    return true;
+}
+
+async function verifyUser(user){
+    const query = `SELECT users.password FROM login.users WHERE users.email='${user.email}'`,
+          [res, ] = await database.sendQuery(query),
+          [salt, key] = res.password.split(':'),
+          hashedBuffer = scryptSync(user.password, salt, 64),
+          keyBuffer = Buffer.from(key, 'hex'),
+          match = timingSafeEqual(hashedBuffer, keyBuffer);
+    return match;
+}
+
+async function getUsers(arg=null){
+    if (arg == null) {
+        query = `SELECT * FROM login.users`;
+        return [res, ] = await database.sendQuery(query);
+    }
+    else if (isNaN(arg)) {
+        query = `SELECT * FROM login.users WHERE users.login='${arg}'`;
+        return [res, ] = await database.sendQuery(query);
+    }
+    else query = `SELECT * FROM login.users WHERE users.uid=${arg}`;
+    return [res, ] = await database.sendQuery(query);
 }
 
 module.exports = {
     addUser,
+    verifyUser,
+    getUsers,
 }
--- server.js
+++ server.js
@@ -2,46 +2,40 @@ if (process.env.NODE_ENV !== 'production'){
     require('dotenv').config();
 }
 const express = require('express'),
-      passport = require('passport'),
       { createHash } = require('./modules/hashing'),
-      { addUser } = require('./modules/user'),
-      { initialize } = require('./modules/passport'),
-      session = require('express-session'),
-      flash = require('express-flash'),
+      { sign } = require('jsonwebtoken'),
+      { addUser, verifyUser, getUsers } = require('./modules/user'),
       app = express(),
-      port = process.env.NODE_PORT,
-      users = [];
-
-initialize(
-    passport,
-    email => users.find(u => u.email === email),
-    id => users.find(u => u.id === id),
-);
+      port = process.env.NODE_PORT;
 
 app.set('view-engine', 'ejs');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(flash());
-app.use(session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false,
-}))
-app.use(passport.initialize());
-app.use(passport.session());
 
 app.get('/login', (req, res)=>{
     res.render('login.ejs');
 });
 
-app.post('/api/login', (req, res)=>{
-    console.table(req.body);
-});// passport.authenticate('local', {
-//     successRedirect: '../panel',
-//     failureRedirect: '../login',
-//     failureFlash: true,
-// } 
-// ));
+app.post('/api/login', async (req, res)=>{
+    try {
+        const user = { email: req.body.email, password: req.body.password },
+              status = await verifyUser(user);
+        if (status == true) {
+            const webToken = sign({ result : req.body.email }, process.env.WEB_TOKEN, { expiresIn: '1h' });
+            res.json(
+                {
+                    success: 1,
+                    message: `Successfully fetched users.`,
+                    token: webToken,
+                }
+            );
+        }
+        else res.redirect('../login');
+    }
+    catch (error) {
+        console.log(error);
+    }
+});
 
 app.get('/register', (req, res)=>{
     res.render('register.ejs');
@@ -50,8 +44,8 @@ app.get('/register', (req, res)=>{
 app.post('/api/register', (req, res)=>{
     try {
         const user = createHash(req.body.email, req.body.login, req.body.password);
-        addUser(user);
-        res.redirect('../login');
+        if (addUser(user) == true) res.redirect('../login');
+        else res.redirect('../register');
     }
     catch {
        res.redirect('../register');
@@ -62,8 +56,30 @@ app.get('/panel', (req, res)=>{
     res.render('panel.ejs');
 });
 
-app.get('/api/users', (req, res)=>{
-    res.json(users);
+app.get('/api/users', async (req, res)=>{
+    res.json(
+        {
+            success: 1,
+            message: `Successfully fetched users.`,
+            data: await getUsers(),
+        }
+    );
+});
+
+app.get('/api/users/:id', async (req, res)=>{
+    const get = await getUsers(req.params.id);
+    if (get.length == 0 || get == false) res.redirect('../../error');
+    else res.json(
+        {
+            success: 1,
+            message: `Successfully fetched user.`,
+            data: get,
+        }
+    );
+});
+
+app.get('/error', (req, res)=>{
+    res.render('error.ejs');
 });
 
 app.listen(port);
