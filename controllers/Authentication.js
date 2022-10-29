278374517c3d718f4775c3d596c8d3e387964e99
botprzemek <botprzemekk@gmail.com>
2022-10-29 13:51:58 +0000

    Authorization fix

--- controllers/Authentication.js
+++ controllers/Authentication.js
@@ -1,14 +1,15 @@
 if (process.env.NODE_ENV !== 'production'){
     require('dotenv').config();
 }
-const regex = {
-          login    : /^[a-zA-Z0-9_-]{8,32}$/,
-          password : /^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[^\w\d\s:])([^\s]){8,24}$/,
-      };
 
 const database = require('../models/Database'),
       crypto = require('crypto'),
-      { verify } = require('jsonwebtoken');
+      { verify } = require('jsonwebtoken'),
+      { logUser } = require('../controllers/Database'),
+      regex = {
+        login    : /^[a-zA-Z0-9_-]{8,32}$/,
+        password : /^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[^\w\d\s:])([^\s]){8,24}$/,
+      };
 
 async function verifyUser(user){
     if ((!user.login.match(regex.login)) || (!user.password.match(regex.password))) return false;
@@ -23,12 +24,33 @@ async function verifyUser(user){
 }
 
 function authUser(req, res, next){
-    const token = req.cookies['token'];
-    const refreshToken = req.cookies['refreshToken'];
-    if (refreshToken == null || token == null) return res.redirect('login');
-    if (refreshToken != null && token == null) return res.redirect('api/token');
-    verify(token, process.env.WEB_TOKEN, (error, udata) =>{
-        if (error) return res.redirect('login');
+    const token = req.cookies['token'],
+          refreshToken = req.cookies['refreshToken'];
+    if (refreshToken == null && token == null) return res.redirect('./../../login');
+    if (refreshToken != null && token == null) return res.redirect('./api/token');
+    if (typeof token !== 'string' || typeof refreshToken !== 'string' || typeof req.cookies['user'] !== 'string'){
+        res.cookie('token', '', { maxAge: -90000, httpOnly: true });
+        res.cookie('refreshToken', '', { maxAge: -90000, httpOnly: true });
+        res.cookie('user', '', { maxAge: -90000, httpOnly: true });
+        return res.redirect('./../../login');
+    }
+    if (JSON.parse(Buffer.from(token.split('.')[1], 'base64').toString()).result !== req.cookies['user']){
+        res.cookie('token', '', { maxAge: -90000, httpOnly: true });
+        res.cookie('refreshToken', '', { maxAge: -90000, httpOnly: true });
+        res.cookie('user', '', { maxAge: -90000, httpOnly: true });
+        return res.redirect('./../../login');
+    }
+    verify(token, process.env.WEB_TOKEN, async (error, udata) =>{
+        if (error) {
+            await logUser({
+                login: req.cookies['user'],
+                data: `User ${req.cookies['user']} tried to change cookies.`,
+            });
+            res.cookie('token', '', { maxAge: -90000, httpOnly: true });
+            res.cookie('refreshToken', '', { maxAge: -90000, httpOnly: true });
+            res.cookie('user', '', { maxAge: -90000, httpOnly: true });
+            return res.redirect('./../../login');
+        }
         req.udata = udata;
         next();
     });
--- routes/User.js
+++ routes/User.js
@@ -1,5 +1,6 @@
 const express = require('express'),
       router = express.Router(),
+      cookieParser = require('cookie-parser'),
       { authUser } = require('../controllers/Authentication'),
       { apiLogin, apiRegister, apiToken, apiUsers, apiUserId } = require('../controllers/Api'),
       { logoutUser } = require('../controllers/User');
@@ -7,7 +8,7 @@ const express = require('express'),
 router.use((req, res, next) => { next() })
 router.get('/', (req, res)=>{ res.redirect('panel') });
 
-router.get('/panel', authUser, (req, res)=>{ res.render('Panel.ejs') });
+router.get('/panel', authUser, (req, res)=>{ res.render('Panel.ejs', { token: req.cookies.token }) });
 router.get('/login', (req, res)=>{ res.render('Login.ejs') });
 router.get('/register', (req, res)=>{ res.render('Register.ejs') });
 
@@ -16,7 +17,7 @@ router.get('/logout', logoutUser);
 router.post('/api/login', apiLogin);
 router.post('/api/register', apiRegister);
 router.get('/api/token', apiToken);
-router.get('/api/users', apiUsers);
-router.get('/api/users/:id', apiUserId);
+router.get('/api/users', authUser, apiUsers);
+router.get('/api/users/:id', authUser, apiUserId);
 
 module.exports = router
