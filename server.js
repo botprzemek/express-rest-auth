7572370a334932cdbc7c4be3cadce3695589f31c
botprzemek <botprzemekk@gmail.com>
2022-10-29 19:45:02 +0000

    modified:   package-lock.json 	modified:   package.json 	modified:   server.js

--- server.js
+++ server.js
@@ -6,15 +6,23 @@ const express = require('express'),
       cookieParser = require('cookie-parser'),
       app = express(),
       routers = require('./routes/User'),
+      rateLimit = require('express-rate-limit'),
+      limiter = rateLimit({
+        windowMs: 15 * 60 * 1000,
+        max: 100,
+        standardHeaders: true,
+        legacyHeaders: false,
+      }),
       port = process.env.NODE_PORT;
 
 app.disable('x-powered-by');
       
 app.set('view-engine', 'ejs');
+
 app.use(express.json({ limit: '1mb' }));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
+app.use(limiter);
 app.use('/', routers);
 
 app.listen(port);
