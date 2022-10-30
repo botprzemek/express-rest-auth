ac0770dc519649ceb35cbaf09494db769e1e2f4a
botprzemek <botprzemekk@gmail.com>
2022-10-30 15:11:06 +0000

    modified:   server.js

--- server.js
+++ server.js
@@ -16,8 +16,9 @@ const express = require('express'),
       port = process.env.NODE_PORT;
 
 app.disable('x-powered-by');
+      
+app.set('view-engine', 'ejs');
 
-app.use(express.static('dist'));
 app.use(express.json({ limit: '1mb' }));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
